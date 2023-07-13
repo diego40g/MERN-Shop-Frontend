f2c19ac6fec27ec2b6ba808df66429c766704aac
diego40g <diegopaznaula@hotmail.com>
2023-07-13 01:41:16 -0500

    change the default component container

--- Shop-Ecommerce/App.tsx
+++ Shop-Ecommerce/App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import AllList from './src/components/AllList';
+import ProductContainer from './src/components/Screens/Products/ProductContainer';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <AllList />
+      <ProductContainer />
     </View>
   );
 }
