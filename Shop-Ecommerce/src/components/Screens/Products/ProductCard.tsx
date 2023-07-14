4380088cf156090c423ba95aa9b7a74c80a37320
diego40g <diegopaznaula@hotmail.com>
2023-07-14 01:37:18 -0500

    delete flex element

--- Shop-Ecommerce/src/components/Screens/Products/ProductCard.tsx
+++ Shop-Ecommerce/src/components/Screens/Products/ProductCard.tsx
@@ -40,7 +40,6 @@ const styles = StyleSheet.create({
     container: {
         width: width / 2 - 20,
         height: width / 1.7,
-        flex: 1,
         padding: 10,
         borderRadius: 10,
         marginTop: 55,
