009f3de47738d6fd35f782e20677cfb5e07f8e48
diego40g <diegopaznaula@hotmail.com>
2023-07-14 23:42:37 -0500

    change all stylesheet to use a width: 50%

--- Shop-Ecommerce/src/components/Screens/Products/ProductList.tsx
+++ Shop-Ecommerce/src/components/Screens/Products/ProductList.tsx
@@ -1,40 +1,20 @@
 import React from 'react';
-import { TouchableOpacity, View, Dimensions, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, Dimensions } from 'react-native';
 
 import ProductCard from './ProductCard';
 
 let { width } = Dimensions.get("window");
 
 const ProductList = (props: any) => {
-    const { data } = props
+    const { item } = props
     return (
-        <View style={styles.container}>
-            {data.map((item: any, index: number) => (
-                <TouchableOpacity key={index} style={styles.item}>
-                    <View style={{ backgroundColor: 'gainsboro' }}>
-                        <ProductCard {...item} />
-                    </View>
-                </TouchableOpacity >
-            ))}
-        </View>
+        <TouchableOpacity style={{ width: '50%' }}>
+            <View style={{ width: width / 2, backgroundColor: 'gainsboro', flex: 1, flexDirection: 'column' }}>
+                <ProductCard {...item} />
+            </View>
+        </TouchableOpacity >
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 10,
-        borderRadius: 5
-    },
-    item: {
-        flexBasis: '50%',
-        padding: 10,
-        borderWidth: 1,
-        borderColor: '#ccc',
-    }
-})
 
 export default ProductList;
