d4f9fd3d5f168d202c9291872bdf4bc439e699ee
diego40g <diegopaznaula@hotmail.com>
2023-07-15 23:59:30 -0500

    product container of search and other components

--- Shop-Ecommerce/src/components/Screens/Products/ProductContainer.tsx
+++ Shop-Ecommerce/src/components/Screens/Products/ProductContainer.tsx
@@ -1,21 +1,34 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
+import { View, StyleSheet, ActivityIndicator, FlatList } from 'react-native';
+import { Container, Header, Icon, Item, Input, Text } from 'native-base';
 
 import ProductList from './ProductList';
 const data = require('../../../../assets/data/products.json')
 
 const ProductContainer = () => {
     const [products, setProducts] = useState([]);
+    const [productsFiltered, setProductsFiltered] = useState([]);
     useEffect(() => {
         setProducts(data);
+        setProductsFiltered(data);
         return () => {
             setProducts([])
         }
     }, [])
     return (
-        <View>
-            <Text>Product Cotainer</Text>
+        <Container>
+            <Header searchBar rounded>
+                <Item>
+                    {/* <Icon name='ios-search' /> */}
+                    <Input
+                        placeholder='Search'
+                    // onFocus={() => { }}
+                    // onChangeText={(text) => { }}
+                    />
+                </Item>
+            </Header>
             <View style={styles.container}>
+                <Text>Product Cotainer</Text>
                 <FlatList
                     numColumns={2}
                     data={products}
@@ -23,13 +36,13 @@ const ProductContainer = () => {
                     keyExtractor={item => item.name}
                 />
             </View>
-        </View>
+        </Container>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        marginTop: 100,
+        marginTop: 10
     }
 })
 
