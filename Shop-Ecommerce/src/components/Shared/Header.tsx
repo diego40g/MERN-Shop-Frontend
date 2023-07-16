9386af2ee4c42b78c341421a18a7411bd29c249b
diego40g <diegopaznaula@hotmail.com>
2023-07-15 23:53:59 -0500

    Creating a header component with logo

---
+++ Shop-Ecommerce/src/components/Shared/Header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet, Image, SafeAreaView, Text } from "react-native";
+
+const Header = () => {
+    return (
+        <SafeAreaView style={styles.header}>
+            <Image
+                source={require("../../../assets/Logo.png")}
+                resizeMode="contain"
+                style={styles.image}
+            />
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        width: "100%",
+        height: 50,
+        flexDirection: "row",
+        alignContent: "center",
+        justifyContent: "center",
+        padding: 20,
+        backgroundColor: "#dcdcdc"
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'contain'
+    }
+})
+
+export default Header;
