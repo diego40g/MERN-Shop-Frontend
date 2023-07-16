9d5982ad9601e283a889a5eec04fa278f672db4a
diego40g <diegopaznaula@hotmail.com>
2023-07-15 23:59:10 -0500

    adding and calling the header

--- Shop-Ecommerce/App.tsx
+++ Shop-Ecommerce/App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './src/components/Shared/Header';
 import ProductContainer from './src/components/Screens/Products/ProductContainer';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <ProductContainer />
     </View>
   );
