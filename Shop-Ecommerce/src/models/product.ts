a0523206f6497e2ac56b81ffbaf24dc1b8f2a609
diego40g <diegopaznaula@hotmail.com>
2023-07-17 00:55:38 -0500

    creating a model to products

---
+++ Shop-Ecommerce/src/models/product.ts
@@ -0,0 +1,18 @@
+const Product = {
+    name: String,
+    description: String,
+    richDescription: String,
+    image: String,
+    images: String,
+    brand: String,
+    price: Number,
+    //category: 
+    countInStock: Number,
+    rating: Number,
+    numReviews: Number,
+    isFeatured: Boolean,
+    dateCreated: Date
+}
+
+
+export default Product;
